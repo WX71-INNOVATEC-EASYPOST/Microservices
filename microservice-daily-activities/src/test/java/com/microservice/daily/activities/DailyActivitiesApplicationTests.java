575d4797da874d73d6a89357b0770b39a6562c39
Miguel Angel Ybañez Esquerre <109547479+Miguel080902@users.noreply.github.com>
2024-11-11 21:57:00 -0500

    first commit

---
+++ microservice-daily-activities/src/test/java/com/microservice/daily/activities/DailyActivitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.daily.activities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyActivitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
